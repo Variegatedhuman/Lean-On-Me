03eb662bb16b61e77fee51a4fc2131db89aa2cd7
Muna Zeki <munazeki@Munas-MacBook-Pro.local>
2023-03-16 19:33:23 -0500

    update

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -3,7 +3,6 @@ const { Post, User, Comment } = require('../models');
 const { Sequelize } = require("sequelize");
 
 router.get('/', (req, res) => {
-<<<<<<< HEAD
   // Render the homepage template
 
 
@@ -13,9 +12,6 @@ router.get('/', (req, res) => {
   res.render('search',{
     markerList: ["full list"]
   });
-=======
-  res.render('search');
->>>>>>> edf87f45f5df5567bc89d1ea2b437680f08e7808
 });
 
 router.get('/login', (req, res) => {
