ff5150e2194060e9281efb1033e8b95543b13f00
Ryebread5555 <Rypetty55@gmail.com>
2023-03-16 21:11:07 -0500

    added location, age, gender, and language

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,29 +1,36 @@
-
 async function signupFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+  const location = document.querySelector('#location-signup').value.trim();
+  const age = document.querySelector('#age-signup').value.trim();
+  const gender = document.querySelector('#gender-signup').value.trim();
+  const language = document.querySelector('#language-signup').value.trim();
 
-    if (username && email && password) {
-        const response = await fetch('/users', {
+  if (name && email && password && location && age && gender && language) {
+      const response = await fetch('/users', {
           method: 'post',
           body: JSON.stringify({
-            username,
-            email,
-            password
+              name,
+              email,
+              password,
+              location,
+              age,
+              gender,
+              language
           }),
           headers: { 'Content-Type': 'application/json' }
-        }); 
+      });
 
-    if (response.ok) {
-        console.log('success');
-        document.location.replace('/profile');
+      if (response.ok) {
+          console.log('success');
+          document.location.replace('/profile');
       } else {
-        alert(response.statusText);
+          alert(response.statusText);
       }
-    }
+  }
 }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler); 
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
